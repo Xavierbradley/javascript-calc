c91a2478b6ba2f056240226c0b96631f3ef52a18
Xavier Bradley <xbradley@rlliquidators.com>
2021-09-16 15:37:12 -0700

    Added styles.css and tweaked the HTML a bit will finish adding the buttons in the next commit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 function display(val)
+{
+  document.getElementById("textval").value+=val
+}
+function evaluate()
+{
+  let x = document.getElementById("textval").value
+  let y = eval(x)
+  document.getElementById("textval").value
+}
+function clr()
+{
+  document.getElementById("textval").value = ""
+}
